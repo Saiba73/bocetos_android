88ebb293c33b6a66719b6db9355d48f06fa6f9ce
unknown <jesus.herreradelgadillo.30350@gmail.com>
2025-04-02 08:36:15 -0600

    agre un poco de diseño a la pantalla de starwars

--- clon_fulanito/app/src/main/java/com/example/clon_fulanito/ui/pantallas/principales/star_wars/pantalla_naves_espaciales.kt
+++ clon_fulanito/app/src/main/java/com/example/clon_fulanito/ui/pantallas/principales/star_wars/pantalla_naves_espaciales.kt
@@ -1,8 +1,12 @@
 package com.example.clon_fulanito.ui.pantallas.principales.star_wars
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,8 +35,24 @@ fun PantallaNavesEspaciales(modifier: Modifier){
             LazyColumn {
                 items(pagina_actual!!.results){ nave_espacial ->
                     Text("NAVE: ${nave_espacial.name}")
+                    Text("Modelo: ${nave_espacial.model}")
+                    HorizontalDivider()
                 }
             }
+
+            Row{
+                Text("Pagina siguiente", modifier = Modifier.clickable {
+                        Log.v("STARWARS", "Pagina siguiente de naves")
+                    }
+                )
+
+                Text("Pagina anterior",
+                    modifier = Modifier.clickable {
+                        Log.v("STARWARS", "Pagina anterior de naves")
+                    })
+            }
+
+
         }
     }
 }
